c22bf1f2f82c6aa80421f2a7cbaf49759069bcf9
Martha Ibanez <ibanezmartha9@gmail.com>
2023-11-07 11:57:52 -0500

    cy and page file were add for the context menu functionality

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -14,6 +14,10 @@ class homePage {
     navigateToCheckboxesPage(){
         return cy.get(':nth-child(6) > a')
     }
+
+    navigateToContextMenu(){
+        return cy.get('//*[@id="content"]/ul/li[7]/a')
+    }
 }
 
 export default homePage
